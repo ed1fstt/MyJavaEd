516c9840d1f51d1d1f6a806128dac546f5159426
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-09-25 23:09:44 +0500

    it'just next session (array)

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,16 @@
 /**
- * Created by shamil on 18.09.2017.
- */
+ * Created by shamil on 25.09.2017
+ * */
 
 //how many lucky tickets we have 
 public class Main {
-    //devide two numbers
-    //return summ of number
-    private static int f(int num){
-        int sum = 0;
-        while (num!=0){
-            sum = sum+(num%10);
-            num/=10;
-        }
-        return sum;
-    }
 
     public static void main(String[] args) {
-        int qttt=0;
-        int number1;
-        int number2;
-        for (int i=1001;i<=999999;i++){
-            //div
-            number1=i%1000;
-            number2=i/1000;
-            if (f(number1)==f(number2)){qttt++;}
-        }
-        System.out.println(qttt);
+        int[] a;
+        for (int i=0; i< a.length; i++){
+            a[i] =
 
+        }
 
     }
 }
