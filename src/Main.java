25d7e47ed1037f8b907b579749d2174c71e22ddf
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-09-26 07:48:07 +0500

    just first task

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
  * Created by shamil on 25.09.2017
  * */
 
-//how many lucky tickets we have 
+//simple array tricks
 public class Main {
 
     public static void main(String[] args) {
-        int[] a;
+        int[] a = new int[10];
         for (int i=0; i< a.length; i++){
-            a[i] =
-
+            a[i] =2*i+2;
+        }
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]);
+        }
+        System.out.println();
+        int b=0;
+        while (b<a.length){
+            System.out.println(a[b]);
+            b++;
         }
 
     }
