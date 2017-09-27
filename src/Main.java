e370451fe8cabbabeab4fa0459d798ce9f06bdbd
ed1fstt <shamilkurmaev@gmail.com>
2017-09-27 17:31:56 +0500

    compleated task#2 array
    hi ho

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,25 @@
  * Created by shamil on 25.09.2017
  * */
 
-//simple array tricks
+//simple array tricks #2
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = new int[10];
-        for (int i=0; i< a.length; i++){
-            a[i] =2*i+2;
+        int a = 0;
+        for(int i = 1; i<=99; i++){
+            if(i%2!=0){a++;}
         }
-        for (int i=0;i<a.length;i++){
-            System.out.print(a[i]);
+        int[] mas = new int[a];
+        for (int i=1, b=0;i<=99;i++){
+            if (i%2!=0){
+                mas[b]=i;
+                System.out.print(mas[b]+" ");
+                b++;
+            }
+            }
+            System.out.println();
+        for(int i=0; i<mas.length; i++){
+            System.out.println(mas[i]);
         }
-        System.out.println();
-        int b=0;
-        while (b<a.length){
-            System.out.println(a[b]);
-            b++;
-        }
-
     }
 }
