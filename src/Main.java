fa359ab09b2c22734ac1c5ce21203e3ff293434e
ed1fstt <shamilkurmaev@gmail.com>
2017-09-28 17:39:15 +0500

    array4task not complete
    1

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,15 @@
  * Created by shamil on 28.09.2017
  * */
 
-//simple array tricks #3
+//simple array tricks #4
 public class Main {
 
     public static void main(String[] args) {
-       int a= 15;
-       int[] arr = new int[a];
-       for(int i=0;i<a; i++){
-           arr[i] = (int)Math.floor(Math.random()*10);
-           System.out.print(arr[i]+" ");
+       int n =8;
+       int[] arr = new int[n];
+       for (int i=0;i<n;i++){
+           arr[i] = (int)Math.floor()
        }
-       int qtt = 0;
-       for (int i=0;i<a;i++){
-           if (arr[i]%2==0){
-               qtt++;
-           }
-       }
-        System.out.println();
-        System.out.println(qtt);
+
     }
 }
