cefba64fd01ecc983382a8142f190da1f6da2ae0
ed1fstt <shamilkurmaev@gmail.com>
2017-09-28 10:18:56 +0500

    completed task 3 theme "array"
    it was somethere mistake - commit from HOME did not right appeared in WORK

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,24 @@
 /**
- * Created by shamil on 25.09.2017
+ * Created by shamil on 28.09.2017
  * */
 
-//simple array tricks #2
+//simple array tricks #3
 public class Main {
 
     public static void main(String[] args) {
-        int a = 0;
-        for(int i = 1; i<=99; i++){
-            if(i%2!=0){a++;}
-        }
-        int[] mas = new int[a];
-        for (int i=1, b=0;i<=99;i++){
-            if (i%2!=0){
-                mas[b]=i;
-                System.out.print(mas[b]+" ");
-                b++;
-            }
-            }
-            System.out.println();
-        for(int i=0; i<mas.length; i++){
-            System.out.println(mas[i]);
-        }
+       int a= 15;
+       int[] arr = new int[a];
+       for(int i=0;i<a; i++){
+           arr[i] = (int)Math.floor(Math.random()*10);
+           System.out.print(arr[i]+" ");
+       }
+       int qtt = 0;
+       for (int i=0;i<a;i++){
+           if (arr[i]%2==0){
+               qtt++;
+           }
+       }
+        System.out.println();
+        System.out.println(qtt);
     }
 }
