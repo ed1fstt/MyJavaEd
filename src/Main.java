d8830dd9838193022ca727fdd7bdf29452c2fba4
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-09-29 08:00:54 +0500

    completed 4th task in array section

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,17 @@ public class Main {
        int n =8;
        int[] arr = new int[n];
        for (int i=0;i<n;i++){
-           arr[i] = (int)Math.floor()
+           arr[i] = (int)Math.floor(Math.random()*10+1);
+           System.out.print(arr[i]+" ");
+       }
+       for (int i=0;i<n;i++){
+           if(i%2==0){
+               arr[i] =0;
+           }
+       }
+       System.out.println();
+       for (int i = 0;i<n;i++){
+           System.out.print(arr[i]+" ");
        }
 
     }
