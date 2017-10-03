39e8d1b7d7139f51198d782562b37a8597d12bbf
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-10-03 21:46:30 +0500

    completed 9th task

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,55 @@
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 /**
  * Created by shamil on 03.10.2017
  * */
 
 //simple array tricks #9
 public class Main {
-
+    //    private static DecimalFormat df2 = new DecimalFormat(".##");
     public static void main(String[] args) {
-        int n=10;
+        int n = 10;
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
+        int an = 0;
         double[] arr3 = new double[n];
-        for (int i=0;i<n;i++){
-            arr1[i] =(int)Math.floor(Math.random()*9+1);
-            arr2[i] =(int)Math.floor(Math.random()*9+1);
-            arr3[i] = arr1/arr2;
+        for (int i = 0; i < n; i++) {
+            arr1[i] = (int) Math.floor(Math.random() * 9 + 1);
+            arr2[i] = (int) Math.floor(Math.random() * 9 + 1);
+            arr3[i] = (double) arr1[i] / arr2[i];
+            if (arr3[i] % 1 == 0) {
+                an++;
+            }
+//            arr3[i] = df2.format((double)arr3[i]);
+
         }
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(arr1[i] + " ");
+//            if (i == n - 1) {
+//                System.out.println();
+//            }
+//            System.out.print(arr2[i] + " ");
+//            if (i == n - 1) {
+//                System.out.println();
+//            }
+//            System.out.print(arr3[i] + " ");
+//            if (i == n - 1) {
+//                System.out.println();
+//            }
+//        }
+
+        System.out.print(Arrays.toString(arr1));
+        System.out.println();
+        System.out.print(Arrays.toString(arr2));
+        System.out.println();
+        System.out.print(Arrays.toString(arr3));
+        System.out.println();
+        System.out.print(an);
 
     }
+
+
 }
+
