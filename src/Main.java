b94d7de4edda66275e3e46d890e71bc8fec0e519
ed1fstt <shamilkurmaev@gmail.com>
2017-10-03 17:50:28 +0500

    completed task#8
    completed task

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,29 @@
  * Created by shamil on 03.10.2017
  * */
 
-//simple array tricks #7
+//simple array tricks #8
 public class Main {
 
     public static void main(String[] args) {
-        int n = 20;
+        int n=12;
+        int m=0;
+        int mi=0;
         int[] arr = new int[n];
-        arr[0]=1;
-        arr[1]=1;
-        for (int i=0; i<n;i++) {
-            if (i != 0 && i != 1) {
-                arr[i] = arr[i - 1] + arr[i - 2];
-            }
-        }
-        for (int i=0;i<n;i++){
+        for (int i=0; i<n;i++){
+            arr[i] =(int)Math.floor(Math.random()*31-15);
             System.out.print(arr[i]+" ");
         }
+        m=arr[0];
+        mi=0;
+        for (int i=0;i<n;i++){
+            if (arr[i]>m){
+                m=arr[i];
+                mi=i;
+            }
+        }
+        System.out.println();
+        System.out.print(m);
+        System.out.println();
+        System.out.print(mi);
     }
 }
