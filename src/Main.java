bae5f8f5165e8b64e74af87597da44552a406a94
ed1fstt <shamilkurmaev@gmail.com>
2017-10-03 16:20:36 +0500

    completed task6
    completed task

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
 /**
- * Created by shamil on 28.09.2017
+ * Created by shamil on 03.10.2017
  * */
 
-//simple array tricks #4
+//simple array tricks #6
 public class Main {
 
     public static void main(String[] args) {
-       int n =8;
-       int[] arr = new int[n];
-       for (int i=0;i<n;i++){
-           arr[i] = (int)Math.floor()
-       }
-
+        boolean ans = false;
+        int n=4;
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] =(int)Math.floor(Math.random()*90+10);
+            System.out.print(arr[i]+" ");
+        }
+        for (int i=0;i<n;i++){
+            if (arr[i]<arr[i+1]){
+                ans =true;
+                if (i==n-2){
+                    break;
+                }
+            }
+            else {
+                ans=false;
+                break;
+            }
+        }
+        System.out.println();
+        if (ans==true){
+            System.out.print("Sequence is increasing");
+        }else
+            System.out.print("Sequence is not increasing(chaotic or decreasing)");
     }
 }
