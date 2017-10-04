9e838247039d6ad3240bbec36b8e037691550c3a
ed1fstt <shamilkurmaev@gmail.com>
2017-10-04 17:28:37 +0500

    not completed 11th task

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         if (sc.hasNextInt()){
-            int n =
+            int n = sc.nextInt();
+            if (n>0 && n%2==0){
+                int[] arr = new int[n];
+                for (int i=0;i<n;i++){
+                    arr[i] = (int)Math.floor(Math.random()*11-5);
+                    System.out.print(arr[i]+" ");
+                }
+            }
 
         }
     }
