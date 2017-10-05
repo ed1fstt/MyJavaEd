4ddf05db9d2a718f0af33f343fb2bb0104cbd750
ed1fstt <shamilkurmaev@gmail.com>
2017-10-05 13:58:56 +0500

    completed 11th task
    just completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Created by shamil on 03.10.2017
+ * Created by shamil on 05.10.2017
  */
 
 //simple array tricks #11
@@ -9,17 +9,41 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()){
-            int n = sc.nextInt();
-            if (n>0 && n%2==0){
-                int[] arr = new int[n];
-                for (int i=0;i<n;i++){
-                    arr[i] = (int)Math.floor(Math.random()*11-5);
-                    System.out.print(arr[i]+" ");
-                }
+        do {
+            System.out.println();
+            System.out.println("Enter dimension of the array");
+            if (sc.hasNextInt()) {
+                int n = sc.nextInt();
+                int sum1 = 0;
+                int sum2 = 0;
+                if (n > 0 && n % 2 == 0) {
+                    int[] arr = new int[n];
+                    for (int i = 0; i < n; i++) {
+                        arr[i] = (int) Math.floor(Math.random() * 11 - 5);
+                        System.out.print(arr[i] + " ");
+                    }
+                    //first half
+                    for (int i = 0; i < n / 2; i++) {
+                        sum1 = sum1 + arr[i];
+                    }
+                    //second half
+                    for (int i = n / 2; i < n; i++) {
+                        sum2 = sum2 + arr[i];
+                    }
+                    if (sum1 > sum2) {
+                        System.out.println();
+                        System.out.println("First half is bigger " + sum1);
+                    } else if (sum2 > sum1) {
+                        System.out.println();
+                        System.out.println("Second half is bigger " + sum2);
+                    } else {
+                        System.out.println();
+                        System.out.println("Two halfs are equal " + sum1);
+                    }
+                } else
+                    System.out.println("PLEASE, ENTER CORRECT NUMBER");
             }
-
         }
+        while (true);
     }
 }
-
