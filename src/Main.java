4e4682dbe1044be17d2a134cf4988d8e2ec1d556
ed1fstt <shamilkurmaev@gmail.com>
2017-10-07 15:14:20 +0500

    notCompleted task13

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,41 @@
+import java.util.Scanner;
+
 /**
- * Created by shamil on 05.10.2017
+ * Created by shamil on 07.10.2017
  */
-//simple array tricks #12
+//simple array tricks #13
 public class Main {
-    private static int getRanPlus() {
+    private static int getRandomValue(int ext) {
         int a = 0;
-        a = (int) Math.floor(Math.random() * 11);
-        return a;
-    }
-
-    private static int getRanMinus() {
-        int a = 0;
-        a = (int) Math.floor(Math.random() * 11 - 10);
-        return a;
-    }
-
-    private static int getIndex() {
-        int a = 0;
-        a = (int) Math.floor(Math.random() * 11);
+        a = (int) Math.floor(Math.random() * (ext + 1));
         return a;
     }
 
     public static void main(String[] args) {
-        int n = 12;
-        int[] arr = new int[n];
-        int[] arrTemp = new int[n];
-        for (int i = 0; i < arrTemp.length; i++) {
-            arrTemp[i] = arrTemp.length;
-        }
-        int h = 0;
-        boolean gotIt = false;
+        int n = 0;
+        boolean fin = false;
         do {
-            //minus
-            for (int i = 0; i < n / 2; i++) {
-                int temp = 0;
-                do {
-                    temp = getRanMinus();
+            System.out.println("Please, enter natural number greater then number 3");
+            Scanner sc = new Scanner(System.in);
+            if (sc.hasNextInt()) {
+                int a = sc.nextInt();
+                if (a > 3) {
+                    n = a;
+//                    System.out.print(" " + n);
+//                    System.out.println();
+                    int[] arr = new int[n];
+                    for (int i = 0; i < arr.length; i++) {
+                        arr[i] = getRandomValue(n);
+                        System.out.print(arr[i] + " ");
+                    }
+                    fin = true;
+                } else {
+                    System.out.println("Error, enter correct value: natural number greater then number 3");
                 }
-                while (temp == 0);
-                arr[i] = temp;
-            }
-            //plus
-            for (int i = n / 2; i < n; i++) {
-                int temp = 0;
-                do {
-                    temp = getRanPlus();
-                }
-                while (temp == 0);
-                arr[i] = temp;
-                gotIt = true;
+            } else {
+                System.out.println("Error,enter correct value");
             }
         }
-        while (gotIt != true);
-
-        for (int i = 0; i < n; i++) {
-            int index = getIndex();
-            for (int j = 0; j < arrTemp.length; j++) {
-                if (index == arrTemp[j]) {
-                    index = (int) (Math.random() * arrTemp.length);
-                    j = -1;
-                }
-            }
-            arrTemp[h] = index;
-            h++;
-            System.out.print(arr[index] + " ");
-        }
+        while (fin == false);
     }
 }
