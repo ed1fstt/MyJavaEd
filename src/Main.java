b260d0e4588f32a62f50dfdc1019f43758fc2d5b
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-10-23 23:40:19 +0500

    working bubble sorting algorithm for simple array

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,39 @@
 /**
  * Created by shamil on 23.10.2017
  */
-//array sorting example
+//bubble sorting in simple array exemple
 public class Main {
-    private static int getRandomValue(int ext){
-        int a = 0;
-        a = (int)Math.floor(Math.random()*(ext+1));
-        return  a;
-    }
+
     public static void main(String[] args) {
-        //array initialization
         int[] a = new int[10];
-        for(int i =0;i<a.length;i++){
-            a[i] =getRandomValue(10);
-            System.out.print(a[i]+" ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = getRandomValue(10);
+            System.out.print(a[i] + " ");
         }
-        //array sorting
-        for (int i=0;i<a.length;i++){
-            int min = a[i];
-            int imin = i;
-            for (int j=i+1;j<a.length;j++){
-                if(a[j]<min){
-                    min = a[j];
-                    imin = j;
+
+        for (int i = a.length - 1; i >= 2; i--) {
+            boolean sorted = true;
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    sorted = false;
                 }
             }
-            if (i!=imin){
-                int temp = a[i];
-                a[i] = a[imin];
-                a[imin] = temp;
+            if (sorted) {
+                break;
             }
-
         }
         System.out.println();
-        //array output
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
+    }
 
+    private static int getRandomValue(int i) {
+        int a = 0;
+        a = (int) Math.floor(Math.random() * i + 1);
+        return a;
     }
 }
