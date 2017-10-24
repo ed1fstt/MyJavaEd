921320871f1b79e57045a6bd92b79d48fce19983
ed1fstt <shamilkurmaev@gmail.com>
2017-10-24 16:49:22 +0500

    compl arra1 task2
    completed array1 task2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,37 @@
+import sun.org.mozilla.javascript.internal.regexp.SubString;
+
 /**
- * Created by shamil on 18.09.2017.
+ * Created by shamil on 24.10.2017.
  */
 
-//how many lucky tickets we have 
+//2 dimensional array init with random numbers from [-99;99]
 public class Main {
-    //devide two numbers
-    //return summ of number
-    private static int f(int num){
-        int sum = 0;
-        while (num!=0){
-            sum = sum+(num%10);
-            num/=10;
-        }
-        return sum;
+
+    private static int getRandomValue() {
+        int a = 0;
+        a = (int) Math.floor(Math.random() * 199 -99);
+        return a;
     }
 
     public static void main(String[] args) {
-        int qttt=0;
-        int number1;
-        int number2;
-        for (int i=1001;i<=999999;i++){
-            //div
-            number1=i%1000;
-            number2=i/1000;
-            if (f(number1)==f(number2)){qttt++;}
+        int maxv = -99;
+        int[][] da = new int[5][8];
+        for (int i = 0; i < da.length; i++) {
+            for (int j = 0; j < da[i].length; j++) {
+                da[i][j] = getRandomValue();
+                if (da[i][j]>=maxv){
+                    maxv = da[i][j];
+                }
+            }
         }
-        System.out.println(qttt);
 
+        for (int i = 0; i < da.length; i++) {
+            for (int j = 0; j < da[i].length; j++) {
+                System.out.print(da[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print(maxv);
 
     }
 }
