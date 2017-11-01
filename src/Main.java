7ae9f1c86b7d3feecff6feb150a210848323b27d
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-11-01 07:53:50 +0500

    done with a little bug

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import sun.org.mozilla.javascript.internal.regexp.SubString;
-
 /**
  * Created by shamil on 24.10.2017.
  */
@@ -9,29 +7,37 @@ public class Main {
 
     private static int getRandomValue() {
         int a = 0;
-        a = (int) Math.floor(Math.random() * 199 -99);
+        a = (int) Math.floor(Math.random() * 11 - 5);
         return a;
     }
 
     public static void main(String[] args) {
-        int maxv = -99;
-        int[][] da = new int[5][8];
+        int[] aMult = new int[7];
+        int[][] da = new int[7][4];
+        int maxIndex = -1;
+        int maxRow = -226;
         for (int i = 0; i < da.length; i++) {
+            int aMultTemp = 1;
             for (int j = 0; j < da[i].length; j++) {
                 da[i][j] = getRandomValue();
-                if (da[i][j]>=maxv){
-                    maxv = da[i][j];
-                }
+                aMultTemp = da[i][j] * aMultTemp;
             }
+            aMult[i] = (int) Math.abs(aMultTemp);
         }
-
         for (int i = 0; i < da.length; i++) {
             for (int j = 0; j < da[i].length; j++) {
                 System.out.print(da[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.print(maxv);
-
+        for (int i = 0; i < aMult.length; i++) {
+            if (aMult[i] > maxRow) {
+                maxRow = aMult[i];
+                maxIndex = i;
+            }
+            System.out.print(aMult[i] + " ");
+        }
+        System.out.println();
+        System.out.print(maxRow + " " + maxIndex);
     }
 }
