cf105cd29257f8a6baf1d85014bd3ce5654613a5
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-11-18 19:58:24 +0500

    completed task1 static methods

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,25 @@
-//*** Created by shamil on 04.11.2017.
-
-
-//multiplicational array
+//*** Created by shamil 18.11.2017
+//static methods
 public class Main {
-    private static int getRandomValue() {
-        int a = 0;
-        a = (int) Math.floor(Math.random() * 9);
-        return a;
+    public static int randomGen(int a, int b){
+        int n=0;
+        if (a==0) {
+            n = (int) Math.floor(Math.random() * Math.abs(b + 1) + a);
+        }
+        else if (a>0){
+            n = (int) Math.floor(Math.random() * Math.abs(b - 1) + a);
+        }
+        else if (a<0){
+            n = (int) Math.floor(Math.random() *(b-a+1) + a);
+        }
+        return n;
     }
-
-    private static int getRandomIndex() {
-        int b = 0;
-        b = (int) Math.floor(Math.random() * 8);
-        return b;
-    }
-
     public static void main(String[] args) {
-        //**********initialize mult array****************
-        int[][] aMult = new int[8][8];
-        String[][] aMultString = new String[8][8];
-        int[] aTask = new int[15];
-        String[] aTaskString = new String[15];
-        for (int i = 0; i < aTask.length; i++) {
-            aTask[i] = -1;
-        }
-        for (int i = 0; i < aMult.length; i++) {
-            for (int j = 0; j < aMult[i].length; j++) {
-                aMult[i][j] = (i + 2) * (j + 2);
-                aMultString[i][j]=(i + 2) +"*"+ (j + 2);
-            }
-        }
-        for (int i = 0; i < aMult.length; i++) {
-            for (int j = 0; j < aMult[i].length; j++) {
-                System.out.print(aMult[i][j] + " ");
-            }
-            System.out.println();
-        }
-        //**********fill array with unique values
-        int n = 0;
-        while (n != 15) {
-            int temp = 0;
-            int indexOfi =getRandomIndex();
-            int indexOfj =getRandomIndex();
-            temp = aMult[indexOfi][indexOfj];
-            for (int i = 0; i < aTask.length; i++) {
-                if (aTask[i] == temp) {
-                    temp = 0;
-                }
-            }
-            if (temp != 0) {
-                aTask[n] = temp;
-                aTaskString[n] =aMultString[indexOfi][indexOfj];
-                n++;
-            }
-        }
-        System.out.println();
-        for (int i = 0; i < aTask.length; i++) {
-            System.out.print(aTask[i] + " ");
-        }
-        System.out.println();
-        for(int i=0; i<aTaskString.length;i++){
-            System.out.print(aTaskString[i]+" ");
-        }
+         int[] ar = new int[40];
+    for (int i=0;i<ar.length;i++){
+        ar[i] = randomGen(10,15);
+        System.out.print(ar[i]+" ");
+    }
+    System.out.println();
     }
 }
