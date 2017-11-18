667de964f1e9f7995c0f1a4454bf1c723943613d
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-11-18 21:52:06 +0500

    completed task#2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,32 @@
 //*** Created by shamil 18.11.2017
-//static methods
+//static methods 2
 public class Main {
-    public static int randomGen(int a, int b){
-        int n=0;
-        if (a==0) {
+    public static void printAr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int randomGen(int a, int b) {
+        int n = 0;
+        if (a == 0) {
             n = (int) Math.floor(Math.random() * Math.abs(b + 1) + a);
-        }
-        else if (a>0){
+        } else if (a > 0) {
             n = (int) Math.floor(Math.random() * Math.abs(b - 1) + a);
-        }
-        else if (a<0){
-            n = (int) Math.floor(Math.random() *(b-a+1) + a);
+        } else if (a < 0) {
+            n = (int) Math.floor(Math.random() * (b - a + 1) + a);
         }
         return n;
     }
+
     public static void main(String[] args) {
-         int[] ar = new int[40];
-    for (int i=0;i<ar.length;i++){
-        ar[i] = randomGen(10,15);
-        System.out.print(ar[i]+" ");
-    }
-    System.out.println();
+        for (int j = 0; j < 5; j++) {
+            int[] ar = new int[10];
+            for (int i = 0; i < ar.length; i++) {
+                ar[i] = randomGen(1, 9);
+            }
+            printAr(ar);
+        }
     }
 }
